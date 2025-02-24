cf9be6d24242158cd5317cd17c0c58a4fdd6f52b
Shevyn Buhary <shevynbuhary@gmail.com>
2025-02-23 22:28:45 -0500

    added event handler to the list group module

--- my-first-react-app/src/components/ListGroup.tsx
+++ my-first-react-app/src/components/ListGroup.tsx
@@ -1,14 +1,20 @@
 import { Fragment } from "react";
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //cities = [];
+  //cities = []; //testing purposes (DO NOT DELETE!!!!!)
   return (
     <>
       <h1>Cities</h1>
       {cities.length === 0 && <p>No city was found</p>}
       <ul className="list-group">
         {cities.map((city) => (
-          <li key={city}>{city}</li>
+          <li
+            className="list-group-item"
+            key={city}
+            onClick={() => console.log("Clicked")}
+          >
+            {city}
+          </li>
         ))}
       </ul>
     </>
