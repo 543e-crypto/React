0fc3448d6328df50ce423224f7e9e9cf58df95a8
Shevyn Buhary <shevynbuhary@gmail.com>
2025-03-24 12:40:50 -0400

    React native song app first commit

--- my-first-react-app/src/components/ListGroup.tsx
+++ my-first-react-app/src/components/ListGroup.tsx
@@ -1,20 +1,19 @@
 import { Fragment } from "react";
 function ListGroup() {
   let cities = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  //cities = []; //testing purposes (DO NOT DELETE!!!!!)
   return (
     <>
       <h1>Cities</h1>
       {cities.length === 0 && <p>No city was found</p>}
       <ul className="list-group">
         {cities.map((city) => (
-          <li
+          <button
             className="list-group-item"
             key={city}
             onClick={() => console.log("Clicked")}
           >
             {city}
-          </li>
+          </button>
         ))}
       </ul>
     </>
